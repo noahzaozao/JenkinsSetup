61158fb41ce03baf76eee84390c4caad147cc993
Noah <234082230@qq.com>
2015-09-15 14:56:52 +0800

    init

---
+++ TestProject/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ant -file build.xml \
+-DPROJECT_NAME="TestProject" \
+-DBASE_FOLDER="/Users/noah/Projects/AdobeAIRProjects/TestProject" \
+-DBUILD_DIR="build"
+#-DBASE_FOLDER="/Users/noah/Projects/unity3dProjects/TestProject"
