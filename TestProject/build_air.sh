cb74f6738dff4f069be1c9323fa6c94ffb2e209a
Noah <234082230@qq.com>
2015-09-16 16:26:07 +0800

    update separate air & unity

--- TestProject/build_air.sh
+++ TestProject/build_air.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-ant -file build.xml \
+ant -file build_air.xml \
+-DDEV_PLATFORM="Air" \
 -DPROJECT_NAME="TestProject" \
 -DBASE_FOLDER="/Users/noah/Projects/AdobeAIRProjects/TestProject" \
 -DBUILD_DIR="build"
+# -DDEV_PLATFORM="Unity" \
 #-DBASE_FOLDER="/Users/noah/Projects/unity3dProjects/TestProject"
---
+++ TestProject/build_unity.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ant -file build_unity.xml \
+-DPROJECT_NAME="TestProject" \
+-DBASE_FOLDER="/Users/noah/Projects/unity3dProjects/TestProject"
